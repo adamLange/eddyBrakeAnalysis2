8783d51ba6200e2804319f3946352da531bea170
Adam Lange <adam.lange.0000@gmail.com>
2015-12-15 21:22:40 -0600

    f vs h plot working now

--- plotFvsV.py
+++ plotFvsV.py
@@ -7,29 +7,35 @@ exportDir = r'C:\Users\adam\Documents\eddyBrake\Analysis2\export'
 
 pFile = re.compile(r'[0-9]*\.csv')
 
-fvsh = pd.read_csv(exportDir+r'\legend.csv')
+fvsv = pd.read_csv(exportDir+r'\legend.csv')
+#fvsv = fvsv.sort(['h','v'])
 
-for i in range(len(fvsh)):
+for i in range(len(fvsv)):
 
-  fname = exportDir+'\\'+str(fvsh.iloc[i].name)+'.csv'
+  fname = exportDir+'\\'+str(int(fvsv.iloc[i]['name']))+'.csv'
   try:
-    df = pd.read_csv(fname) #'
+    df = pd.read_csv(fname) #
 
     fx = df.iloc[-1]['Force1.Force_x [newton]']
     fy = df.iloc[-1]['Force1.Force_y [newton]']
 
-    fvsh.loc[i,'F_x'] = fx
-    fvsh.loc[i,'F_y'] = fy
+    fvsv.loc[i,'F_x'] = fx
+    fvsv.loc[i,'F_y'] = fy
   except:
     print('{} does not exist, skipping'.format(fname))
 
 
-plt.scatter(fvsh['v'],fvsh['F_x'],label='F_x')
+for h in fvsv['h'].unique():
+  df = fvsv[fvsv['h']==h]
+  df = df.sort_values(['v'])
+  print(df)
+  plt.plot(df['v'],df['F_x'],'-',label='h = {}'.format(h))
+  plt.plot(df['v'],df['F_y'],'--',label='h = {}'.format(h))
 
-"""
-
-plt.xlabel('time [s]')
-plt.ylabel('Force [n]')
+plt.legend()
+plt.title('- F_x, -- F_y\n h [m]')
+plt.xlabel('Velocity [ms]')
+plt.ylabel('Force [N]')
+plt.grid()
 plt.show()
 
-"""
